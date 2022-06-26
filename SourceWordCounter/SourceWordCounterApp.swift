f709486526a36d1db8b402c2269c2ee079287b2f
Ryan J. W. Kim <tamielryan@gmail.com>
2022-06-26 19:02:38 +0300

    Initial Commit

---
+++ SourceWordCounter/SourceWordCounterApp.swift
@@ -0,0 +1,20 @@
+//
+//  SourceWordCounterApp.swift
+//  SourceWordCounter
+//
+//  Created by Ryan J. W. Kim on 2022/06/26.
+//
+
+import SwiftUI
+
+@main
+struct SourceWordCounterApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
